4b8eeb5c1ea168e04de9e9a6d20c447a40c64769
lvzhiwei <492227300@qq.com>
2024-03-06 18:22:27 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <nav>
+  <nav class="tip_one">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
-
-<style>
+<style lang="scss">
 #app {
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 nav {
+  width: 100px;
+  height: 30px;
   padding: 30px;
 }
 
 nav a {
-  font-weight: bold;
   color: #2c3e50;
+  font-weight: bold;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
